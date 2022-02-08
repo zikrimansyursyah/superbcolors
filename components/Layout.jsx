5a05311edac81a200ba80d87692dadc7173145cf
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2022-02-08 16:53:34 +0700

    remove header and re-set min-h-screen

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,11 +5,12 @@ import Navbar from "./module/Navbar";
 export default function Layout({ children }) {
   return (
     <>
-      <Header pageTitle="Pick your Fav Colors" />
       <Navbar />
       <div className="min-h-screen bg-[url(/bg.webp)] bg-cover bg-no-repeat">
-        <div className="w-full min-h-screen glass">
-          <div className="container mx-auto pt-32 pb-20">{children}</div>
+        <div className="w-full glass">
+          <div className="container mx-auto min-h-screen pt-32 pb-20">
+            {children}
+          </div>
           <Footer />
         </div>
       </div>
