403c4c684b63483e33c9b2867cb76a3a66fbf57d
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2022-02-08 16:52:45 +0700

    update flexibility content with filter categories

--- components/ContentCard.jsx
+++ components/ContentCard.jsx
@@ -1,18 +1,31 @@
 import Card from "./module/Card";
 import colorData from "../data/color";
 
-export default function ContentCard() {
+export default function ContentCard({ categories }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-10 mt-5 px-6 sm:px-0">
-      {(colorData.data || []).map((data) => (
-        <Card
-          key={data.name}
-          name={data.name}
-          image={data.image}
-          liked={data.liked}
-          colors={data.colors}
-        />
-      ))}
+      {categories && categories !== "all"
+        ? (colorData.data || []).map(
+            (data) =>
+              data.categories === categories && (
+                <Card
+                  key={data.name}
+                  name={data.name}
+                  image={data.image}
+                  liked={data.liked}
+                  colors={data.colors}
+                />
+              )
+          )
+        : (colorData.data || []).map((data) => (
+            <Card
+              key={data.name}
+              name={data.name}
+              image={data.image}
+              liked={data.liked}
+              colors={data.colors}
+            />
+          ))}
     </div>
   );
 }
