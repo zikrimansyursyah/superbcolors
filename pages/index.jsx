5b29b1dc45a44d70a6077f0ad7230fde9b4b9e2a
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2022-02-08 16:56:27 +0700

    add header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import ContentCard from "../components/ContentCard";
+import Header from "../components/Header";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
+      <Header pageTitle="Pick your fav Color" />
       <ContentCard />
     </Layout>
   );
